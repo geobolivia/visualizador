7349752040b95e79303fa6e615d9b0623d0407df
Sylvain Lesage <severo@rednegra.net>
2012-09-07 12:39:47 -0400

    init() lanzada con el evento onLoad, y createLayout() para las dims.
    init() esta lanzada con el evento onLoad, y lanza todo el resto del
    script
    
    createLayout() define el tamaño de los <div>
    
    Nota: pasamos el codigo javascript al check jslint para asegurarse
    que la sintaxis es correcta
    http://www.jslint.com

--- api/geo.js
+++ api/geo.js
@@ -1,3 +1,33 @@
-(function() {
+var document, window;
 
-})();
+(function () {
+  "use strict";
+
+  var createLayout, init;
+
+  /**
+   * Method: createLayout
+   * Set the size of all <div> elements
+   *
+   * Returns:
+   * nothing
+   */
+  createLayout = function () {
+    document.getElementById('map').style.width = '100%';
+    document.getElementById('map').style.height = '100%';
+  };
+
+  /**
+   * Method: init
+   * Principal function launched on "onLoad" event
+   *
+   * Returns:
+   * nothing
+   */
+  init = function () {
+    createLayout();
+  };
+
+  window.onload = init;
+
+}());
